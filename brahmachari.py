a22f6773832c4af199b2f5e38ea5ce84ab6325d7
sanket.p@ezdi.us-at-205008539362 <kQ7M8Uy7TAptIZLj7U3BpILFIpXwWGjRzk8TiD2aPAs=>
2020-04-24 11:31:33 +0530

    script added

---
+++ brahmachari.py
@@ -0,0 +1,37 @@
+import discord
+import random
+import string
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    
+    bad_words = ["bc", "mc", "chutiya", "lund","gand","chu","asshole","chodu","lodu","randi","loda","bhangi","fuck","fcuk","fuk","aukat"
+    ,"laude","lavde","bhangi","madarc","bhen","wtf","pikina","tf","maa ki","bsdk","rndi","gandu","mader","suck","bitch"]
+
+    msg_list = [" your message was deleted"," you are disguisting."," saale gaali kisko bola?", " sorry we don't speak that here. :)"," gaado nai bolvani. #peace"," yeh koi r@#dikha#a hai?",
+    " Gaandu salaa!"," <-- Ashleel hai yeh launda"]
+    
+    table = str.maketrans(dict.fromkeys(string.punctuation))
+    content = message.content.translate(table)    
+    content = content.replace(" ","")
+    content = content.lower()
+    
+    for word in bad_words:
+        
+        if word in content:
+            await message.channel.purge(limit=1)
+            usr = str(message.author)
+            usr = usr[:usr.index('#')]
+            msgs = []
+            msg = await message.channel.send(usr + random.choice(msg_list) + " \n No gaali coz i'm brahmachari. \n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+            msgs.append(msg)
+            await asyncio.sleep(3)
+            await message.channel.delete_messages(msgs)
+            #await message.channel.send("No gaali coz i'm brahmachari.")
+            #await message.channel.send("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+client.run("NTkzNzgyMDY0OTE5MDg1MDg5.XqFMJg.RFnvOQ_E_qYmbUElXZPfRqCB56Q")
+
